2b38d51f5b9d46415306b569b24cba93cd4605f7
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2022-05-05 20:58:06 +0800

    add ts, chakra, formik and yup to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React from 'react'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        <h1>Helloooooooo</h1>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
